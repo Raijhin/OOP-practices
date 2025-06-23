43de3eaa6455e01c54c5e8d4aa4da6726660149d
Md. Zihan Zaman Razin <168391545+Raijhin@users.noreply.github.com>
2025-06-23 18:27:00 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    public static void main(String[] args) {
+        greeting();
+        Account artosAccount = new Account("artosAccount",100.00);
+        Account artosSwissAccount = new Account("artosSwissAccount",1000.00);
+
+    }
+
+    public static void greeting() {
+        System.out.println("Greetings from the method world!");
+    }
+    }
